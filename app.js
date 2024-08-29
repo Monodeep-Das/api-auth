70a2085ea8f02d18b46640ed46bc92fd8c9e9ba9
Monodeep-Das <m.monodas@gmail.com>
2024-08-29 23:02:21 +0530

    :0

--- app.js
+++ app.js
@@ -4,32 +4,38 @@ const createError = require('http-errors');
 require('dotenv').config();
 require('./helpers/init_mongodb');
 const AuthRoute = require('./Routes/Auth.route');
-const { verifyAccessToken } = require('./helpers/jwt_helper');
 const client = require('./helpers/init_redis');
-
-// client.SET('foo', 'baa');
-// client.GET('foo', (err, value) => {
-//     if (err) console.log(err.message)
-//     console.log(value);
-// }); 
+const { verifyAccessToken } = require('./helpers/jwt_helper');
 
 const app = express();
+
+// Ensure Redis is connected before performing any operations
+client.on('ready', () => {
+    console.log('Redis is ready to use');
+    
+    // Setting and getting a value from Redis
+    client.SET('foo', 'baa', (err, reply) => {
+        if (err) console.log(err.message);
+        console.log('SET reply:', reply);  // Confirm SET operation
+    });
+
+    client.GET('foo', (err, value) => {
+        if (err) console.log(err.message);
+        console.log('GET value:', value);  // Retrieve the value of 'foo'
+    });
+});
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', verifyAccessToken, async(req, res, next) => {
-
+app.get('/', verifyAccessToken, async (req, res, next) => {
     res.send('Hello From Express');
 });
 
 app.use('/auth', AuthRoute);
 
-app.use(async(req, res, next) => {
-    // const error = new Error('Not Found');
-    // error.status = 404;
-    // next(error);
+app.use(async (req, res, next) => {
     next(createError.NotFound());
 });
 
